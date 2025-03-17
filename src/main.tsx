dae47f379a972a26c7db4a0e9c61ee55edbcaf48
OleksandrKirkov <kirkovsasha009@gmail.com>
2025-03-18 01:26:41 +0200

    feat: add base screens (homa, auth) and setting routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
+import { pageConfig } from './config/page.config'
+import { Home } from './screens/home/Home'
+import { Auth } from './screens/auth/Auth'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<App />} />
+                <Route path={pageConfig.home} element={<Home />} />
+                <Route path={pageConfig.auth} element={<Auth />} />
             </Routes>
-            <App />
         </BrowserRouter>
     </StrictMode>,
 )
