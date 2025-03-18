49626407015523e30ba034945fc13cfda36b624b
OleksandrKirkov <kirkovsasha009@gmail.com>
2025-03-19 00:50:46 +0200

    feat(main.tsx): add layout wrappers in routes

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,19 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 import { pageConfig } from './config/page.config'
 import { Home } from './screens/home/Home'
 import { Auth } from './screens/auth/Auth'
+import { ProtectedRoutes } from './routes/ProtectedRoutes'
+import { RedirectIfAuth } from './routes/RedirectIfAuth'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <BrowserRouter>
             <Routes>
-                <Route path={pageConfig.home} element={<Home />} />
-                <Route path={pageConfig.auth} element={<Auth />} />
+                <Route element={<ProtectedRoutes />}>
+                    <Route index element={<Home />} />
+                </Route>
+                <Route element={<RedirectIfAuth />}>
+                    <Route path={pageConfig.auth} element={<Auth />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     </StrictMode>,
