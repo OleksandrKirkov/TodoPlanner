a248d58b80c4163677201695a7debd4ad119d7a2
OleksandrKirkov <kirkovsasha009@gmail.com>
2025-03-19 00:50:25 +0200

    feat(Auth.tsx): update Auth page

--- src/screens/auth/Auth.tsx
+++ src/screens/auth/Auth.tsx
@@ -1,3 +1,9 @@
 export function Auth() {
-    return <div>Auth</div>
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <div className="bg-slate-800 p-8 rounded-lg shadow-md">
+                <h2 className="font-semibold mb-4">Auth</h2>
+            </div> 
+        </div>
+    )
 }
